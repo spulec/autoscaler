eed330d699bff4e8594ef6fd26b0c2851bc8e41a
Steve Pulec <spulec@gmail.com>
2013-08-05 09:33:49 -0400

    0.0.2

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='autoscaler',
-    version='0.0.1',
+    version='0.0.2',
     description='AutoScaler makes it easier to create and manage a large '
                 'number of Launch Configurations and AutoScaling Groups',
     author='Steve Pulec',
